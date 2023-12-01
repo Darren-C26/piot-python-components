ddf3a54e0bb99fdae0b5aa9d24807004b04234eb
Darren-C26 <decons2602@gmail.com>
2023-12-01 01:55:13 -0500

    Update MqttClientConnector.py

--- src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
@@ -9,12 +9,14 @@
 
 import logging
 import paho.mqtt.client as mqttClient
+import ssl
 
 import programmingtheiot.common.ConfigConst as ConfigConst
 
 from programmingtheiot.common.ConfigUtil import ConfigUtil
 from programmingtheiot.common.IDataMessageListener import IDataMessageListener
 from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
+from programmingtheiot.data.DataUtil import DataUtil
 
 from programmingtheiot.cda.connection.IPubSubClient import IPubSubClient
 
@@ -25,6 +27,17 @@ class MqttClientConnector(IPubSubClient):
 	"""
 
 	def __init__(self, clientID: str = None):
+		"""
+		Default constructor. This will set remote broker information and client connection
+		information based on the default configuration file contents.
+		
+		@param clientID Defaults to None. Can be set by caller. If this is used, it's
+		critically important that a unique, non-conflicting name be used so to avoid
+		causing the MQTT broker to disconnect any client using the same name. With
+		auto-reconnect enabled, this can cause a race condition where each client with
+		the same clientID continuously attempts to re-connect, causing the broker to
+		disconnect the previous instance.
+		"""
 		self.config = ConfigUtil()
 		self.dataMsgListener = None
 		
@@ -43,25 +56,19 @@ class MqttClientConnector(IPubSubClient):
 		self.defaultQos = \
 			self.config.getInteger( \
 				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.DEFAULT_QOS_KEY, ConfigConst.DEFAULT_QOS)
+		self.enableEncryption = \
+			self.config.getBoolean( \
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.ENABLE_CRYPT_KEY)
 		
+		self.pemFileName = \
+			self.config.getProperty( \
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.CERT_FILE_KEY)
+			
 		self.mqttClient = None
+		self.clientID=clientID
 		
-		# IMPORTANT:
-		# 
-		# You can choose to set clientID in a number of ways:
-		#  1 - use the locationID value in PiotConfig.props as the clientID (see below)
-		#  2 - pass a custom clientID into constructor (from DeviceDataManager or your test)
-		#  3 - hard code a clientID in this constructor (generally not recommended)
-		#  4 - if using Python Paho, set NO client ID and let broker auto-assign
-		#      a random value (not recommended if setting clean session flag to False)
-	
-		# TODO: the following is just a sample; use your own unique ID
 		if not clientID:
-			self.clientID = \
-				self.config.getProperty( \
-					ConfigConst.CONSTRAINED_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY)
-		
-		# TODO: be sure to validate the clientID!
+			self.clientID = self.config.getProperty( ConfigConst.CONSTRAINED_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY)
 			
 		logging.info('\tMQTT Client ID:   ' + self.clientID)
 		logging.info('\tMQTT Broker Host: ' + self.host)
@@ -73,6 +80,18 @@ class MqttClientConnector(IPubSubClient):
 			# TODO: make clean_session configurable
 			self.mqttClient = mqttClient.Client(client_id = self.clientID, clean_session = True)
 			
+			try:
+				if self.enableEncryption:
+					logging.info("Enabling TLS encryption...")
+					
+					self.port = \
+						self.config.getInteger( \
+							ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.SECURE_PORT_KEY, ConfigConst.DEFAULT_MQTT_SECURE_PORT)
+		
+					self.mqttClient.tls_set(self.pemFileName, tls_version = ssl.PROTOCOL_TLSv1_2)
+			except:
+				logging.warning("Failed to enable TLS encryption. Using unencrypted connection.")
+	
 			self.mqttClient.on_connect = self.onConnect
 			self.mqttClient.on_disconnect = self.onDisconnect
 			self.mqttClient.on_message = self.onMessage
@@ -103,21 +122,31 @@ class MqttClientConnector(IPubSubClient):
 			return False
 		
 	def onConnect(self, client, userdata, flags, rc):
-		logging.info('MQTT client connected to broker: ' + str(client))
+		logging.info('[Callback] Connected to MQTT broker. Result code: ' + str(rc))
+		
+		# NOTE: Be sure to set `self.defaultQos` during instantiation!
+		self.mqttClient.subscribe( \
+			topic = ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE.value, qos = self.defaultQos)
+		
+		self.mqttClient.message_callback_add( \
+			sub = ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE.value, \
+			callback = self.onActuatorCommandMessage)
 		
 	def onDisconnect(self, client, userdata, rc):
-		logging.info('MQTT client disconnected from broker: ' + str(client))
+		pass
+		#logging.info('MQTT client disconnected from broker: ' + str(client))
 		
 	def onMessage(self, client, userdata, msg):
 		payload = msg.payload
-		
+	
 		if payload:
 			logging.info('MQTT message received with payload: ' + str(payload.decode("utf-8")))
 		else:
 			logging.info('MQTT message received with no payload: ' + str(msg))
-			
+				
 	def onPublish(self, client, userdata, mid):
-		logging.info('MQTT message published: ' + str(client))
+		pass
+		#logging.info('MQTT message published: ' + str(client))
 	
 	def onSubscribe(self, client, userdata, mid, granted_qos):
 		logging.info('MQTT client subscribed: ' + str(client))	
@@ -135,10 +164,19 @@ class MqttClientConnector(IPubSubClient):
 		@param userdata The user reference context.
 		@param msg The message context, including the embedded payload.
 		"""
-		pass
+		logging.info('[Callback] Actuator command message received. Topic: %s.', msg.topic)
 	
-	def publishMessage(self, resource: ResourceNameEnum = None, msg: str = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
-		# check validity of resource (topic)
+		if self.dataMsgListener:
+			try:
+				# assumes all data is encoded using UTF-8 (between GDA and CDA)
+				actuatorData = DataUtil().jsonToActuatorData(msg.payload.decode('utf-8'))
+				
+				self.dataMsgListener.handleActuatorCommandMessage(actuatorData)
+			except:
+				logging.exception("Failed to convert incoming actuation command payload to ActuatorData: ")
+	
+	def publishMessage(self, resource: ResourceNameEnum = None, msg: str = None, qos: int = ConfigConst.DEFAULT_QOS):
+			# check validity of resource (topic)
 		if not resource:
 			logging.warning('No topic specified. Cannot publish message.')
 			return False
@@ -147,18 +185,22 @@ class MqttClientConnector(IPubSubClient):
 		if not msg:
 			logging.warning('No message specified. Cannot publish message to topic: ' + resource.value)
 			return False
-		
+					
 		# check validity of QoS - set to default if necessary
 		if qos < 0 or qos > 2:
 			qos = ConfigConst.DEFAULT_QOS
 		
 		# publish message, and wait for publish to complete before returning
 		msgInfo = self.mqttClient.publish(topic = resource.value, payload = msg, qos = qos)
-		msgInfo.wait_for_publish()
 		
+		# The next SLOC is commented out now - recall it was added in Lab Module 06
+		#msgInfo.wait_for_publish()
+		
+		# NOTE: The 'True' return no longer guarantees successful publish,
+		# as it will return before the publish may successfully complete
 		return True
 	
-	def subscribeToTopic(self, resource: ResourceNameEnum = None, callback = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
+	def subscribeToTopic(self, resource: ResourceNameEnum = None, callback = None, qos: int = ConfigConst.DEFAULT_QOS):
 		# check validity of resource (topic)
 		if not resource:
 			logging.warning('No topic specified. Cannot subscribe.')
@@ -169,7 +211,7 @@ class MqttClientConnector(IPubSubClient):
 			qos = ConfigConst.DEFAULT_QOS
 		
 		# subscribe to topic
-		logging.info('Subscribing to topic %s', resource.value)
+		#logging.info('Subscribing to topic %s', resource.value)
 		self.mqttClient.subscribe(resource.value, qos)
 		
 		return True
@@ -180,11 +222,11 @@ class MqttClientConnector(IPubSubClient):
 			logging.warning('No topic specified. Cannot unsubscribe.')
 			return False
 		
-		logging.info('Unsubscribing to topic %s', resource.value)
+		#logging.info('Unsubscribing to topic %s', resource.value)
 		self.mqttClient.unsubscribe(resource.value)
 		
 		return True
 
 	def setDataMessageListener(self, listener: IDataMessageListener = None):
-		if listener:
-			self.dataMsgListener = listener
+	    if listener:
+	        self.dataMsgListener = listener
